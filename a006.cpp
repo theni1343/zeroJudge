6670441184511eee32ea3fb3a421b29fb37a6b94
theni1343 <47775446+theni1343@users.noreply.github.com>
2019-04-06 16:22:08 +0800

    Add files via upload

---
+++ a006.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+int a,b,c,x1,x2;
+double D,I;
+
+while(cin>>a>>b>>c){
+D=b*b-4*a*c;
+
+if(D>0){
+x1=(-b+sqrt(D))/2*a;
+x2=(-b-sqrt(D))/2*a;
+cout<<"Two different roots x1="<<x1<<" , x2="<<x2<<endl;
+}
+
+else if(D==0){
+x1=-b/2*a;
+cout<<"Two same roots x="<<x1<<endl;
+}
+
+else{
+cout<<"No real root"<<endl;
+}
+}
+    return 0;
+}
